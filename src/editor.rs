3ed679fe3fce8be57b296ca622635777074a863c
Jack Donnelly <jack@jackdonnelly.dev>
2022-12-08 13:05:23 +0000

    Refactoring

--- src/editor.rs
+++ src/editor.rs
@@ -22,16 +22,16 @@ impl Editor {
                     Key::Ctrl('q') => break,
                     _ => println!("{:?}\r", key),
                 },
-                Err(err) => die(err),
+                Err(err) => die(&err),
             }
         }
     }
 
     pub fn default() -> Self {
-        Editor {}
+        Self {}
     }
 }
 
-fn die(e: std::io::Error){
+fn die(e: &std::io::Error){
     panic!("{}",e);
 }
