37366b8067705a1476b4f6e812a7636c34a59e75
Jack Donnelly <jack@jackdonnelly.dev>
2022-12-08 22:19:47 +0000

    Adding cursor show and hide functions

--- src/editor.rs
+++ src/editor.rs
@@ -32,6 +32,7 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error>{
+        Terminal::cursor_hide();
         Terminal::clear_screen();
         if self.should_quit {
             println!("Goodbye.\r");
@@ -39,6 +40,7 @@ impl Editor {
             self.draw_rows();
             Terminal::cursor_position(0,0);
         }
+        Terminal::cursor_show();
         Terminal::flush()
     }
 
