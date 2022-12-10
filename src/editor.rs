a757ee9d02cbdce7102ad6bb020472ccab523779
Jack Donnelly <jack@jackdonnelly.dev>
2022-12-10 15:55:20 +0000

    Updating prompt

--- src/editor.rs
+++ src/editor.rs
@@ -103,6 +103,23 @@ impl Editor {
         Terminal::flush()
     }
 
+    fn save(&mut self){
+        if self.document.file_name.is_none(){
+            let new_name = self.prompt("Save as: ").unwrap_or(None);
+            if new_name.is_none(){
+                self.status_message = StatusMessage::from("Save Aborted.".to_string());
+                return;
+            }
+            self.document.file_name = new_name;
+        }
+
+        if self.document.save().is_ok(){
+            self.status_message = StatusMessage::from("File saved successfully".to_string());
+        }else {
+            self.status_message = StatusMessage::from("Error writing file!".to_string());
+        }
+    }
+
     fn draw_status_bar(&self){
         let mut status; 
         let width = self.terminal.size().width as usize;
@@ -144,17 +161,7 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
-            Key::Ctrl('s') => {
-                if self.document.file_name.is_none() {
-                    self.document.file_name = Some(self.prompt("Save as: ")?);
-                }
-
-                if self.document.save().is_ok(){
-                    self.status_message = StatusMessage::from("File saved successfully".to_string());
-                }else {
-                    self.status_message= StatusMessage::from("Error writing to file!".to_string());
-                }
-            },
+            Key::Ctrl('s') => self.save(),
             Key::Char(c) => {
                 self.document.insert(&self.cursor_position, c);
                 self.move_cursor(Key::Right);
@@ -295,24 +302,36 @@ impl Editor {
         }
     }
 
-    fn prompt(&mut self, prompt: &str) -> Result<String, std::io::Error>{
+    fn prompt(&mut self, prompt: &str) -> Result<Option<String>, std::io::Error>{
         let mut result = String::new();
         loop {
             self.status_message = StatusMessage::from(format!("{}{}", prompt, result));
             self.refresh_screen()?;
-            if let Key::Char(c) = Terminal::read_key()?{
-                if c == '\n' {
-                    self.status_message = StatusMessage::from(String::new());
+            match Terminal::read_key()?{
+                Key::Backspace => {
+                    if !result.is_empty(){
+                        result.truncate(result.len() -1);
+                    }
+                },
+                Key::Char('\n') => break,
+                Key::Char(c) => {
+                    if !c.is_control(){
+                        result.push(c);
+                    }
+                },
+                Key::Esc => {
+                    result.truncate(0);
                     break;
-                }
-
-                if !c.is_control(){
-                    result.push(c);
-                }
-            }
+                },
+                _ => (),
+            } 
         }
 
-        Ok(result)
+        self.status_message = StatusMessage::from(String::new());
+        if result.is_empty(){
+            return Ok(None);
+        }
+        Ok(Some(result))
     }
 
     
