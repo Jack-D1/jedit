7334853e1a6a3664c29ebe2f212bd6ca1801868d
Jack Donnelly <jack@jackdonnelly.dev>
2023-01-06 12:40:40 +0000

    Beginnings of filetype detection

---
+++ src/filetype.rs
@@ -0,0 +1,17 @@
+pub struct FileType {
+    name: String,
+    hl_opts: HighlightingOptions,
+}
+#[derive(Default)]
+pub struct HighlightingOptions{
+    pub numbers: bool,
+}
+
+impl Default for FileType {
+    fn default() -> Self {
+        Self {
+            name: String::from("No filetype"),
+            hl_opts: HighlightingOptions::default()
+        }
+    }
+}
