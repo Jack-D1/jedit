dc34a2a94e365b01b532761d7a6df1d88709c714
Jack Donnelly <jack@jackdonnelly.dev>
2023-04-05 16:55:37 +0100

    More highlighting refactor

--- src/row.rs
+++ src/row.rs
@@ -170,7 +170,7 @@ impl Row {
 
         None
     }
-    fn highlight_match(&mut self, word: Option<&str>){
+    fn highlight_match(&mut self, word: &Option<String>){
         if let Some(word) = word{
             if word.is_empty(){
                 return;
@@ -346,7 +346,7 @@ impl Row {
     }
 
     #[allow(clippy::indexing_slicing, clippy::integer_arithmetic)]
-    pub fn highlight(&mut self, opts: &HighlightingOptions, word: Option<&str>, start_with_comment: bool) -> bool{
+    pub fn highlight(&mut self, opts: &HighlightingOptions, word: &Option<String>, start_with_comment: bool) -> bool{
         self.highlighting = Vec::new();
         let chars: Vec<char> = self.string.chars().collect();
         let mut index = 0;
